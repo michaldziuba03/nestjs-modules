7ecdda92ed514b96bf27964dd999f4507f2b08b3
mich4ld <mchldziuba@gmail.com>
2021-10-11 08:52:54 +0200

    fix redis module interfaces

--- redis/src/redis.interface.ts
+++ redis/src/redis.interface.ts
@@ -1,7 +1,7 @@
 import Redis from 'ioredis';
 
 export interface ClientOptions extends Redis.RedisOptions {
-    connectionName: string;
+    connectionName?: string;
 }
 
-export type ModuleOptions = ClientOptions | ClientOptions;
+export type ModuleOptions = ClientOptions | ClientOptions[];
