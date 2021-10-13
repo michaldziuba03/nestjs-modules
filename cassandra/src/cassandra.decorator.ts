2cf820123cef84d9872085b687dd75a5c3f601d4
mich4ld <mchldziuba@gmail.com>
2021-10-13 23:38:16 +0200

    fix optional args for cassandra inject decorators

--- cassandra/src/cassandra.decorator.ts
+++ cassandra/src/cassandra.decorator.ts
@@ -1,9 +1,9 @@
 import { Inject } from '@nestjs/common';
 import { createCassandraToken } from './cassandra.utils';
 
-export const InjectCassandra = (clientName: string) => {
+export const InjectCassandra = (clientName?: string) => {
     const token = createCassandraToken(clientName);
     return Inject(token);
 }
 
-export const injectCassandraToken = (clientName: string) => createCassandraToken(clientName);
+export const injectCassandraToken = (clientName?: string) => createCassandraToken(clientName);
