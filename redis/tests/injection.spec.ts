2f34c50c5e4587c862837a2fd0278bbae1ca226f
mich4ld <mchldziuba@gmail.com>
2021-10-14 17:31:00 +0200

    add token injection tests for redis module

---
+++ redis/tests/injection.spec.ts
@@ -0,0 +1,14 @@
+import { injectRedisToken } from "../src"
+
+describe('Injection token', () => {
+    it('should return default token', () => {
+        const token = injectRedisToken();
+        expect(token).toContain('default');
+    });
+
+    it('should return conn1 token', () => {
+        const clientName = 'conn1';
+        const token = injectRedisToken(clientName);
+        expect(token).toContain(clientName);
+    });
+});
