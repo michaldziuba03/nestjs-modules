e9a9ebaa6e64699d28ad2d90eeeaa84279ec08a9
Michał Dziuba <mail@michaldziuba.dev>
2022-12-15 08:19:57 +0100

    withdrawal of zookeeper connector

--- zookeeper/src/zookeeper.constants.ts
+++
@@ -1,2 +0,0 @@
-export const ZK_CONTEXT = 'ZookeeperModule';
-export const ZK_TOKEN = 'ZK_TOKEN';
--- zookeeper/src/zookeeper.utils.ts
+++
@@ -1,4 +0,0 @@
-import { Logger } from "@nestjs/common";
-import { ZK_CONTEXT } from "./zookeeper.constants";
-
-export const logger = new Logger(ZK_CONTEXT);
